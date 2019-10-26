71a0794d65481d6e653216aadff34714d1e1d825
George <starcalluh@live.com>
2019-10-26 18:34:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,17 +1,1 @@
 # require modules here
-
-def load_library(path)
-  # code goes here
- def load_library(emoticon_file)
-  
-  end
-  emoticon_lib
-end
-
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
-end
